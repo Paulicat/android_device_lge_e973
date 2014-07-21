b4a9b2c82970eff09ed7c23ccc5b3c68e8647c89
Anton Paulic <paulicat@vmbox12>
2014-07-21 08:38:02 -0400

    Fix type in sound files.

--- device.mk
+++ device.mk
@@ -44,9 +44,9 @@ PRODUCT_COPY_FILES += \
     device/lge/e973/ramdisk/fstab.e973:root/fstab.e973 \
     device/lge/e973/Video_Game.ogg:/system/media/audio/ringtones/Video_Game.ogg \
     device/lge/e973/Cloud.ogg:/system/media/audio/notifications/Cloud.ogg \
-    device/lge/e973/IMD_Delete.ogg:/system/media/audio/ui/IMD_Delete.ogg \
-    device/lge/e973/IMD_Spacebar.ogg:/system/media/audio/ui/IMD_Spacebar.ogg \
-    device/lge/e973/IMD_Standard.ogg:/system/media/audio/ui/IMD_Standard.ogg \
+    device/lge/e973/IME_Delete.ogg:/system/media/audio/ui/IME_Delete.ogg \
+    device/lge/e973/IME_Spacebar.ogg:/system/media/audio/ui/IME_Spacebar.ogg \
+    device/lge/e973/IME_Standard.ogg:/system/media/audio/ui/IME_Standard.ogg \
     device/lge/e973/KeypressDelete.ogg:/system/media/audio/ui/KeypressDelete.ogg \
     device/lge/e973/KeypressReturn.ogg:/system/media/audio/ui/KeypressReturn.ogg \
     device/lge/e973/KeypressSpacebar.ogg:/system/media/audio/ui/KeypressSpacebar.ogg \
